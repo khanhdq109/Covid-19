e183128a39437cd4c46277ed1b1c4abc199110ba
Q.Khanh <khanh.daoquoc@hcmut.edu.vn>
2022-03-17 12:26:26 +0700

    12h26 - 17/03/2022

--- IV.R
+++ IV.R
@@ -8,6 +8,7 @@ library("csvread")
 library("readr")
 library("rlist")
 library("lubridate")
+library("lattice")
 
 # Set Data
 setwd("D:/Khanh/BK/HK212/Discrete Structures/CODE")      # path
@@ -24,8 +25,40 @@ c2_data<-subset(data, location == national[2] & new_cases >= 0 & new_deaths >= 0
 c3_data<-subset(data, location == national[3] & new_cases >= 0 & new_deaths >= 0)
 
   # 1/ Bieu do tan so tich luy quoc gia cho cac chau luc
+ctn<-continent[!duplicated(continent)]
+ctn<-na.omit(ctn)
+num_countries<-c()
+for (i in c(1:length(ctn))) {
+  temp<-subset(data, continent == ctn[i])
+  temp<-temp$location
+  temp2<-temp[!duplicated(temp)]
+  num_countries[i]<-length(temp2)
+}
+freq<-c(0, 0, 0, 0, 0)
+for (i in c(1:5)) {
+  for (j in c(1:length(num_countries))) {
+    if (num_countries[j] > i * 10 & num_countries[j] <= (i + 1) * 10) {
+      freq[i]<-freq[i] + 1
+    }
+  }
+}
+cumfreq<-cumsum(freq)
+breaks<-seq(10, 50, by = 10)
+plot(breaks, cumfreq,
+     pch = 19,
+     col = "red",
+     main = "Plot",
+     xlab = "Contries",
+     ylab = "Continents")
+lines(breaks, cumfreq, col = "red")
 
   # 2/ Bieu do tan so tuong doi quoc gia cho cac chau luc
+histogram(num_countries,
+          breaks = 4,
+          col = rgb (67 / 255, 128 / 255, 13 / 255),
+          main = "Relative Frequency Histogram",
+          xlab = "Number of Countries",
+          ylab = "Percent of Total")
 
   # 3/ Bieu do the hien nhiem benh da bao cao cua cac quoc gia trong 7 ngay cuoi cung cua nam cuoi cung
 lastDay<-c()
@@ -80,8 +113,8 @@ outlier_newcase2<-subset(c2_data, c2_data$new_cases < lower_newcase2 | c2_data$n
 count_outlier_newcase2<-sum(table(outlier_newcase2))
 
 IQRnewcase3<-(Q_newcasecountry3[[4]] - Q_newcasecountry3[[2]])
-lower_newcase3<-(Q_newcasecountry3[[2]] - 1.5*IQRnewcase3)
-upper_newcase3<-(Q_newcasecountry3[[4]] + 1.5*IQRnewcase3)
+lower_newcase3<-(Q_newcasecountry3[[2]] - 1.5 * IQRnewcase3)
+upper_newcase3<-(Q_newcasecountry3[[4]] + 1.5 * IQRnewcase3)
 outlier_newcase3<-subset(c3_data, c3_data$new_cases < lower_newcase3 | c3_data$new_cases > upper_newcase3)
 count_outlier_newcase3<-sum(table(outlier_newcase3))
 
@@ -100,8 +133,8 @@ Q_newdeathcountry2<-quantile(c2_data$new_deaths)
 Q_newdeathcountry3<-quantile(c3_data$new_deaths)
 
 IQRnewdeath1<-(Q_newdeathcountry1[[4]] - Q_newdeathcountry1[[2]])
-lower_newdeath1<-(Q_newdeathcountry1[[2]] - 1.5*IQRnewdeath1)
-upper_newdeath1<-(Q_newdeathcountry1[[4]] + 1.5*IQRnewdeath1)
+lower_newdeath1<-(Q_newdeathcountry1[[2]] - 1.5 * IQRnewdeath1)
+upper_newdeath1<-(Q_newdeathcountry1[[4]] + 1.5 * IQRnewdeath1)
 outlier_newdeath1<-subset(c1_data, c1_data$new_deaths < lower_newdeath1 | c1_data$new_deaths > upper_newdeath1)
 count_outlier_newdeath1<-sum(table(outlier_newdeath1))
 
